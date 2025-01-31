6bdf4cd9f3217c9eb38fc2061e7ecca80fdc241e
Bhanu Venkatesh <166484223+lovelybanu@users.noreply.github.com>
2025-01-31 11:06:34 +0530

    Update settings.py

--- blogPosting/settings.py
+++ blogPosting/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i+1j(t993#q9&-1#)w6pvce#4qwmegcq4q6b!&jj7rthyw*whu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
